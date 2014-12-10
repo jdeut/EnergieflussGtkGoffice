f6517c0eedd85dbd2f3cce6de0d9c7c9200b71ba
energienetzwerk Admin <johannes.deutsch@uni-due.de>
2014-12-10 15:57:51 +0100

    remove big arrow and arcs from main canvas

--- src/main.c
+++ src/main.c
@@ -13,38 +13,20 @@ populate_canvas (App * app)
 
     top_level_group = goc_canvas_get_root (app->canvas);
 
-    arr = g_new0 (GOArrow, 1);
-
-    go_arrow_init_kite (arr, 100, 100, 20);
-
-    item =
-        goc_item_new (top_level_group, GOC_TYPE_LINE, "x0",
-                      100.0, "y0", 100.0, "x1", 500.0, "y1",
-                      400.0, "end-arrow", arr, NULL);
-
-    g_object_get (G_OBJECT (item), "style", &style, NULL);
-
-    line = &style->line;
-
-    line->width = 50;
-    line->color = GO_COLOR_FROM_RGBA (0, 200, 0, 255);
-
-    goc_item_new (top_level_group, GOC_TYPE_ARC, "xc", 100.0,
-                  "yc", 100.0, "xr", 10.0, "yr", 10.0, "ang2", 2 * M_PI, NULL);
-    goc_item_new (top_level_group, GOC_TYPE_ARC, "xc", 500.0,
-                  "yc", 400.0, "xr", 10.0, "yr", 10.0, "ang2", 2 * M_PI, NULL);
-
     button = gtk_button_new_with_label ("System 1");
 
     goc_item_new (top_level_group, GOC_TYPE_WIDGET, "widget", button, "x",
                   100.0, "y", 200.0, "width", 100.0, "height", 50.0, NULL);
 
     g_signal_connect (button, "button-press-event",
-                      G_CALLBACK (propagate_button_press_event_to_canvas_cb), app);
+                      G_CALLBACK (propagate_button_press_event_to_canvas_cb),
+                      app);
     g_signal_connect (button, "button-release-event",
-                      G_CALLBACK (propagate_button_release_event_to_canvas_cb), app);
+                      G_CALLBACK (propagate_button_release_event_to_canvas_cb),
+                      app);
     g_signal_connect (button, "motion-notify-event",
-                      G_CALLBACK (propagate_motion_notify_event_to_canvas_cb), app);
+                      G_CALLBACK (propagate_motion_notify_event_to_canvas_cb),
+                      app);
 
     button = gtk_button_new_with_label ("System 2");
 
@@ -52,11 +34,30 @@ populate_canvas (App * app)
                   300.0, "y", 250.0, "width", 100.0, "height", 50.0, NULL);
 
     g_signal_connect (button, "button-press-event",
-                      G_CALLBACK (propagate_button_press_event_to_canvas_cb), app);
+                      G_CALLBACK (propagate_button_press_event_to_canvas_cb),
+                      app);
     g_signal_connect (button, "button-release-event",
-                      G_CALLBACK (propagate_button_release_event_to_canvas_cb), app);
+                      G_CALLBACK (propagate_button_release_event_to_canvas_cb),
+                      app);
     g_signal_connect (button, "motion-notify-event",
-                      G_CALLBACK (propagate_motion_notify_event_to_canvas_cb), app);
+                      G_CALLBACK (propagate_motion_notify_event_to_canvas_cb),
+                      app);
+
+    /* Test of new GocItem */
+
+    item = goc_item_new (top_level_group, MY_TYPE_SYSTEM, NULL);
+
+    g_object_get(item, "widget", &button, NULL);
+
+    g_signal_connect (button, "button-press-event",
+                      G_CALLBACK (propagate_button_press_event_to_canvas_cb),
+                      app);
+    g_signal_connect (button, "button-release-event",
+                      G_CALLBACK (propagate_button_release_event_to_canvas_cb),
+                      app);
+    g_signal_connect (button, "motion-notify-event",
+                      G_CALLBACK (propagate_motion_notify_event_to_canvas_cb),
+                      app);
 }
 
 int
