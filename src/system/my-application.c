1cf9f32639f246dfdcf6d19688f2fbe16dbcd0dc
energienetzwerk Admin <johannes.deutsch@uni-due.de>
2015-01-17 18:55:20 +0100

    let application stand on GtkApplication and some GActions...

---
+++ src/system/my-application.c
@@ -0,0 +1,158 @@
+#include "my-application.h"
+
+/* 'private'/'protected' functions */
+static void my_application_class_init (MyApplicationClass * klass);
+static void my_application_init (MyApplication * self);
+static void my_application_finalize (GObject *);
+static void my_application_dispose (GObject *);
+
+struct _MyApplicationPrivate
+{
+    GtkWidget *window;
+};
+
+#define MY_APPLICATION_GET_PRIVATE(o)      (G_TYPE_INSTANCE_GET_PRIVATE((o), \
+                                       MY_TYPE_APPLICATION, \
+                                       MyApplicationPrivate))
+
+G_DEFINE_TYPE (MyApplication, my_application, GTK_TYPE_APPLICATION);
+
+
+static const GActionEntry my_application_app_entries[] = {
+    {"quit", my_application_quit, NULL, NULL, NULL}
+};
+
+GQuark
+my_application_error_quark (void)
+{
+    return g_quark_from_static_string ("my-application-error-quark");
+}
+
+static void
+startup (GApplication * app)
+{
+
+    G_APPLICATION_CLASS (my_application_parent_class)->startup (app);
+
+    g_action_map_add_action_entries (G_ACTION_MAP (app),
+                                     my_application_app_entries,
+                                     G_N_ELEMENTS (my_application_app_entries),
+                                     app);
+}
+
+static void
+activate (GApplication * application)
+{
+    MyApplication *self = MY_APPLICATION (application);
+
+    /* Create the interface. */
+    if (self->_priv->window == NULL) {
+        gchar *size_str;
+
+        /* Showtime! */
+        interface_create (self);
+
+        gtk_window_set_application (GTK_WINDOW (self->_priv->window),
+                                    GTK_APPLICATION (self));
+        gtk_widget_show_all (self->_priv->window);
+    }
+
+    /* Bring it to the foreground */
+    gtk_window_present (GTK_WINDOW (self->_priv->window));
+}
+
+static void
+constructed (GObject * object)
+{
+    /* Set various properties up */
+    /*g_application_set_application_id (G_APPLICATION (object), "org.gnome.Hitori"); */
+    /*g_application_set_flags (G_APPLICATION (object), G_APPLICATION_HANDLES_COMMAND_LINE); */
+
+    /* Localisation */
+    /*bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR); */
+    /*bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8"); */
+    /*textdomain (GETTEXT_PACKAGE); */
+
+    g_set_application_name ("Energie");
+    gtk_window_set_default_icon_name ("hitori");
+
+    /* Chain up to the parent class */
+    G_OBJECT_CLASS (my_application_parent_class)->constructed (object);
+}
+
+static void
+my_application_class_init (MyApplicationClass * klass)
+{
+    GObjectClass *gobject_class;
+    GApplicationClass *gapplication_class = G_APPLICATION_CLASS (klass);
+
+    gobject_class = G_OBJECT_CLASS (klass);
+
+    g_type_class_add_private (gobject_class, sizeof (MyApplicationPrivate));
+
+    gobject_class->finalize = my_application_finalize;
+    gobject_class->dispose = my_application_dispose;
+    gobject_class->constructed = constructed;
+
+    gapplication_class->startup = startup;
+    gapplication_class->activate = activate;
+    /*gapplication_class->command_line = handle_command_line; */
+}
+
+static void
+my_application_init (MyApplication * self)
+{
+    self->_priv = MY_APPLICATION_GET_PRIVATE (self);
+
+    /* to init any of the private data, do e.g: */
+}
+
+static void
+my_application_dispose (GObject * object)
+{
+    G_OBJECT_CLASS (my_application_parent_class)->dispose (object);
+}
+
+static void
+my_application_finalize (GObject * object)
+{
+    /* free/unref instance resources here */
+    G_OBJECT_CLASS (my_application_parent_class)->finalize (object);
+}
+
+MyApplication *
+my_application_new (void)
+{
+    MyApplication *self;
+
+    self = g_object_new (MY_TYPE_APPLICATION, NULL);
+
+    return self;
+}
+
+void
+my_application_set_window (MyApplication * self, GtkWidget * window)
+{
+
+    g_return_if_fail (MY_IS_APPLICATION (self));
+    g_return_if_fail (GTK_IS_WIDGET (window));
+
+    self->_priv->window = window;
+}
+
+void
+my_application_quit (GSimpleAction * simple, GVariant * parameter,
+                     gpointer data)
+{
+    MyApplication *self = MY_APPLICATION (data);
+
+    if (self->_priv->window != NULL)
+        gtk_widget_destroy (self->_priv->window);
+
+    /*if (hitori->normal_font_desc != NULL) */
+    /*pango_font_description_free (hitori->normal_font_desc); */
+    /*if (hitori->painted_font_desc != NULL) */
+    /*pango_font_description_free (hitori->painted_font_desc); */
+
+    g_application_quit (G_APPLICATION (self));
+}
---
+++ src/system/my-application.h
@@ -0,0 +1,48 @@
+#ifndef __MY_APPLICATION_H__
+#define __MY_APPLICATION_H__
+
+#include <gtk/gtk.h>
+
+
+G_BEGIN_DECLS
+#define MY_TYPE_APPLICATION             (my_application_get_type())
+#define MY_APPLICATION(obj)             (G_TYPE_CHECK_INSTANCE_CAST((obj),MY_TYPE_APPLICATION,MyApplication))
+#define MY_APPLICATION_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST((klass),MY_TYPE_APPLICATION,MyApplicationClass))
+#define MY_IS_APPLICATION(obj)          (G_TYPE_CHECK_INSTANCE_TYPE((obj),MY_TYPE_APPLICATION))
+#define MY_IS_APPLICATION_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE((klass),MY_TYPE_APPLICATION))
+#define MY_APPLICATION_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS((obj),MY_TYPE_APPLICATION,MyApplicationClass))
+#define MY_APPLICATION_ERROR                (my_application_error_quark ())
+typedef struct _MyApplication MyApplication;
+typedef struct _MyApplicationClass MyApplicationClass;
+typedef struct _MyApplicationPrivate MyApplicationPrivate;
+
+struct _MyApplication
+{
+    GtkApplication parent;
+    /* insert public members here */
+
+    /* private */
+    MyApplicationPrivate *_priv;
+};
+
+struct _MyApplicationClass
+{
+    GtkApplicationClass parent_class;
+
+};
+
+GType my_application_get_type (void);
+
+/* fill in public functions */
+MyApplication *
+my_application_new (void);
+
+void 
+my_application_set_window (MyApplication * self, GtkWidget * window);
+
+void
+my_application_quit (GSimpleAction * simple, GVariant * parameter,
+                     gpointer data);
+
+G_END_DECLS
+#endif /* __MY_APPLICATION_H__ */
