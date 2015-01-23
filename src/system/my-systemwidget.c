e92f3bd9dfbe849778c254144c41e2b401bdaeea
energienetzwerk Admin <johannes.deutsch@uni-due.de>
2015-01-23 11:52:04 +0100

    implement optical response if mouse hovers over a MySystemWidget and adjust placing of labels

--- src/system/my-systemwidget.c
+++ src/system/my-systemwidget.c
@@ -17,7 +17,8 @@ struct _MySystemWidgetPrivate
                                        MY_TYPE_SYSTEM_WIDGET, \
                                        MySystemWidgetPrivate))
 
-G_DEFINE_TYPE_WITH_PRIVATE (MySystemWidget, my_system_widget, GTK_TYPE_EVENT_BOX);
+G_DEFINE_TYPE_WITH_PRIVATE (MySystemWidget, my_system_widget,
+                            GTK_TYPE_EVENT_BOX);
 
 
 GQuark
@@ -45,6 +46,25 @@ my_system_widget_class_init (MySystemWidgetClass * klass)
                                                   image);
 }
 
+
+gboolean
+my_system_enter_event (MySystemWidget * self,
+                       GdkEvent * event, gpointer user_data)
+{
+    gtk_widget_set_state_flags(GTK_WIDGET(self), GTK_STATE_FLAG_ACTIVE, TRUE);
+
+    return FALSE;
+}
+
+gboolean
+my_system_leave_event (MySystemWidget * self,
+                       GdkEvent * event, gpointer user_data)
+{
+    gtk_widget_set_state_flags(GTK_WIDGET(self), GTK_STATE_FLAG_NORMAL, TRUE);
+
+    return FALSE;
+}
+
 static void
 my_system_widget_init (MySystemWidget * self)
 {
@@ -54,6 +74,11 @@ my_system_widget_init (MySystemWidget * self)
 
     /*  obj->_priv->_frobnicate_mode = FALSE; */
     gtk_widget_init_template (GTK_WIDGET (self));
+
+    g_signal_connect (self, "enter-notify-event",
+                      G_CALLBACK (my_system_enter_event), NULL);
+    g_signal_connect (self, "leave-notify-event",
+                      G_CALLBACK (my_system_leave_event), NULL);
 
 }
 
@@ -70,22 +95,28 @@ my_system_widget_finalize (GObject * object)
     G_OBJECT_CLASS (my_system_widget_parent_class)->finalize (object);
 }
 
-void my_system_widget_size_allocate_cb(GtkWidget *widget, GdkRectangle *allocation, gpointer data) {
-    g_print("ok\n");
+void
+my_system_widget_size_allocate_cb (GtkWidget * widget,
+                                   GdkRectangle * allocation, gpointer data)
+{
+    g_print ("ok\n");
 }
 
 MySystemWidget *
 my_system_widget_new (void)
-{ 
+{
     MySystemWidget *self;
 
     self = g_object_new (MY_TYPE_SYSTEM_WIDGET, NULL);
 
-    g_signal_connect(self, "size-allocate", G_CALLBACK(my_system_widget_size_allocate_cb), NULL);
+    g_signal_connect (self, "size-allocate",
+                      G_CALLBACK (my_system_widget_size_allocate_cb), NULL);
 
-    self->_priv->pixbuf = gdk_pixbuf_new_from_resource_at_scale ("/org/gtk/myapp/lagerfeuer.png", 200, -1, TRUE, NULL);
+    self->_priv->pixbuf =
+        gdk_pixbuf_new_from_resource_at_scale ("/org/gtk/myapp/lagerfeuer.png",
+                                               200, -1, TRUE, NULL);
 
-    gtk_image_set_from_pixbuf(self->_priv->image, self->_priv->pixbuf);
+    gtk_image_set_from_pixbuf (self->_priv->image, self->_priv->pixbuf);
 
     return self;
 }
